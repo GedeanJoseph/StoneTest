1dc7382bf6b8096cf221129a81f502b1b86fc599
Gedean Santos <gedean.santos@wittel.com>
2020-12-19 20:44:05 -0300

    Lets Say Hello! =P

---
+++ StoneTest.Crawler.App/StoneTest.Crawler.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StoneTest.Crawler.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stone Test - WebCrowler says: \"-Hello world\"!");
+        }
+    }
+}
